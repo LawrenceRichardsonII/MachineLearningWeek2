8223bdc8351246bb0a8d1cface72ae0c3b5b831a
galacticFlaneur <lawrencedr2@gmail.com>
2019-09-06 14:16:11 -0500

    resetting with comments and shows inline, not jupyter or matlab friendly

--- dataset.py
+++ dataset.py
@@ -8,40 +8,30 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import matplotlib as mpl
 
-
 start = datetime.datetime(2010, 1, 1)
 end = datetime.datetime(2017, 1, 11)
 
 df = web.DataReader("AAPL", 'yahoo', start, end)
+#.tail shows only the end, we actually need to do a .print if we want to see in line on cmder
 df.tail()
 
+#construct our moving averages
 close_px = df['Adj Close']
 mavg = close_px.rolling(window=100).mean()
 
-
-
-# Adjusting the size of matplotlib
-
+#format for matlabplot (to print and visulaize)
 mpl.rc('figure', figsize=(8, 7))
 mpl.__version__
 
 # Adjusting the style of matplotlib
 style.use('ggplot')
 
+#this is our dataframe series we pulled in from the datareader, could also be from a csv, give it a label
 close_px.plot(label='AAPL')
+#this was the dataframe manipulation
 mavg.plot(label='mavg')
 plt.legend()
 
 
-rets = close_px / close_px.shift(1) - 1
-rets.plot(label='return')
+plt.show()
 
-dfcomp = web.DataReader(['AAPL', 'GE', 'GOOG', 'IBM', 'MSFT'],'yahoo',start=start,end=end)['Adj Close']
-
-retscomp = dfcomp.pct_change()
-
-corr = retscomp.corr()
-
-plt.scatter(retscomp.AAPL, retscomp.GE)
-plt.xlabel(‘Returns AAPL’)
-plt.ylabel(‘Returns GE’)
