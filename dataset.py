ad975a04c792839dadeb72d5219e5b5daa6250d7
galacticFlaneur <lawrencedr2@gmail.com>
2019-09-06 15:11:26 -0500

    used clfreg, poly and knn then displayed, end of tutoiral

--- dataset.py
+++ dataset.py
@@ -1,14 +1,27 @@
 import pandas as pd
 import datetime
 import pandas_datareader.data as web
+import math
+import numpy as np
 from pandas import Series, DataFrame
 from pandas.plotting import scatter_matrix
+
+from sklearn import tree
 
 
 
 import matplotlib.pyplot as plt
 from matplotlib import style
 import matplotlib as mpl
+
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+from sklearn.pipeline import make_pipeline
 
 start = datetime.datetime(2010, 1, 1)
 end = datetime.datetime(2017, 1, 11)
@@ -66,19 +79,88 @@ corr = retscomp.corr()
 #plt.yticks(range(len(corr)), corr.columns);
 
 #return rate and risk
-plt.scatter(retscomp.mean(), retscomp.std())
-plt.xlabel('Expected returns')
-plt.ylabel('Risk')
-for label, x, y in zip(retscomp.columns, retscomp.mean(), retscomp.std()):
-    plt.annotate(
-        label, 
-        xy = (x, y), xytext = (20, -20),
-        textcoords = 'offset points', ha = 'right', va = 'bottom',
-        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
-        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+#plt.scatter(retscomp.mean(), retscomp.std())
+#plt.xlabel('Expected returns')
+#plt.ylabel('Risk')
+#for label, x, y in zip(retscomp.columns, retscomp.mean(), retscomp.std()):
+#    plt.annotate(
+#        label, 
+#        xy = (x, y), xytext = (20, -20),
+#        textcoords = 'offset points', ha = 'right', va = 'bottom',
+#        bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+#        arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
 
 
+#building dataframe features
+dfreg = df.loc[:,['Adj Close','Volume']]
+dfreg['HL_PCT'] = (df['High'] - df['Low']) / df['Close'] * 100.0
+dfreg['PCT_change'] = (df['Close'] - df['Open']) / df['Open'] * 100.0
 
-plt.legend()
 
+##pre processing the data now
+# Drop missing value
+dfreg.fillna(value=-99999, inplace=True)
+# We want to separate 1 percent of the data to forecast
+forecast_out = int(math.ceil(0.01 * len(dfreg)))
+# Separating the label here, we want to predict the AdjClose
+forecast_col = 'Adj Close'
+dfreg['label'] = dfreg[forecast_col].shift(-forecast_out)
+X = np.array(dfreg.drop(['label'], 1))
+# Scale the X so that everyone can have the same distribution for linear regression
+X = preprocessing.scale(X)
+
+# Finally We want to find Data Series of late X and early X (train) for model generation and evaluation
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+# Separate label and identify it as y
+y = np.array(dfreg['label'])
+y = y[:-forecast_out]
+
+
+# Linear regression
+clfreg = LinearRegression(n_jobs=-1)
+clfreg.fit(X, y)
+# Quadratic Regression 2
+clfpoly2 = make_pipeline(PolynomialFeatures(2), Ridge())
+clfpoly2.fit(X, y)
+
+# Quadratic Regression 3
+clfpoly3 = make_pipeline(PolynomialFeatures(3), Ridge())
+clfpoly3.fit(X, y)
+
+# KNN Regression
+clfknn = KNeighborsRegressor(n_neighbors=2)
+clfknn.fit(X, y)
+
+confidencereg = clfreg.score(X, y)
+confidencepoly2 = clfpoly2.score(X, y)
+confidencepoly3 = clfpoly3.score(X, y)
+confidenceknn = clfknn.score(X, y)
+
+# results
+print('The linear regression confidence is ', confidencereg)
+print('The quadratic regression 2 confidence is ', confidencepoly2)
+print('The quadratic regression 3 confidence is ', confidencepoly3)
+print('The knn regression confidence is ', confidenceknn)
+
+#using clfknn here could be clfpoly etc
+forecast_set = clfreg.predict(X_lately)
+dfreg['Forecast'] = np.nan
+
+
+#plotting our predicitons
+
+last_date = dfreg.iloc[-1].name
+last_unix = last_date
+next_unix = last_unix + datetime.timedelta(days=1)
+
+for i in forecast_set:
+    next_date = next_unix
+    next_unix += datetime.timedelta(days=1)
+    dfreg.loc[next_date] = [np.nan for _ in range(len(dfreg.columns)-1)]+[i]
+dfreg['Adj Close'].tail(500).plot()
+dfreg['Forecast'].tail(500).plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
 plt.show()
